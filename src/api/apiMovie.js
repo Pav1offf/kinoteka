61f434189a0600594f5fce38041f70061b7ea99c
Pavlov <Pav1offYura@yandex.ru>
2025-11-08 16:28:59 +0500

    feat: add movie page and react router

--- src/api/apiMovie.js
+++ src/api/apiMovie.js
@@ -3,6 +3,22 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
 const BASE_API = import.meta.env.VITE_MOVIE_BASE_API_URL;
 
+export const getMovie = async (id = 303) => {
+  try {
+    const response = await axios.get(`${BASE_API}v2.2/films/${id}`, {
+      headers: {
+        "X-API-KEY": API_KEY,
+        "Content-Type": "application/json",
+      },
+      params: {},
+    });
+
+    return response.data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 export const getMovies = async (type = "TOP_POPULAR_ALL", page = 1) => {
   try {
     const response = await axios.get(`${BASE_API}v2.2/films/collections`, {
