0c4cb36cd10fec338d08885a5162f83275317730
Pavlov <Pav1offYura@yandex.ru>
2025-11-21 23:27:21 +0500

    feat: add search by filters on the main page

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -5,9 +5,17 @@ import { countries } from "../../api/countries";
 import { useContext } from "react";
 import { ThemeContext } from "../../context/ThemeContext";
 
-const SideBar = () => {
+const SideBar = ({
+  selectedCountry,
+  setSelectedCountry,
+  selectedGenre,
+  setSelectedGenre,
+  selectedYear,
+  setSelectedYear,
+}) => {
   const { theme } = useContext(ThemeContext);
   const { isOpen } = useContext(ThemeContext);
+
   const navigate = useNavigate();
 
   const navigateToLists = () => {
@@ -23,18 +31,57 @@ const SideBar = () => {
       <button className={styles.listButton} onClick={() => navigateToLists()}>
         Списки
       </button>
-      <select name="country">
-        <option value={"all"}>{"Все страны"}</option>
-        {countries.map((item) => {
-          return <option value={item}>{item}</option>;
+      <select
+        name="countries"
+        value={selectedCountry}
+        onChange={(e) => setSelectedCountry(e.target.value)}
+      >
+        <option value={""}>{"Все страны"}</option>
+        {countries.map((item, i) => {
+          return (
+            <option value={item.id} key={i}>
+              {item.country}
+            </option>
+          );
         })}
       </select>
-      <select name="genre">
-        <option value={"all"}>{"Все жанры"}</option>
-        {genres.map((item) => {
-          return <option value={item.genre}>{item.genre}</option>;
+      <select
+        name="genres"
+        value={selectedGenre}
+        onChange={(e) => setSelectedGenre(e.target.value)}
+      >
+        <option value={""}>{"Все жанры"}</option>
+        {genres.map((item, i) => {
+          return (
+            <option value={item.id} key={i}>
+              {item.genre}
+            </option>
+          );
         })}
       </select>
+      <select
+        name="years"
+        value={selectedYear}
+        onChange={(e) => setSelectedYear(e.target.value)}
+      >
+        <option value={""}>{"Все годы"}</option>
+        {Array.from({ length: 6 }).map((_, i) => {
+          return (
+            <option value={2025 - i} key={i}>
+              {2025 - i}
+            </option>
+          );
+        })}
+        {Array.from({ length: 15 }).map((_, i) => {
+          return (
+            <option value={`${2010 - i * 10}-${2020 - i * 10 - 1}`} key={i}>
+              {2010 - i * 10}-{2020 - i * 10 - 1}
+            </option>
+          );
+        })}
+      </select>
+
+      <p>{selectedYear}</p>
     </div>
   );
 };
